f081ed2506dbe989e18de5c6e137f93ba87c8426
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2024-10-20 03:52:18 +0530

    ui update

--- src/components/Repo/Repo.tsx
+++ src/components/Repo/Repo.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import React from "react";
 import {
   Card,
@@ -8,7 +9,6 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "../ui/button";
-import { Link } from "react-router-dom";
 
 interface RepoProps {
   label: string;
@@ -22,27 +22,27 @@ const Repo: React.FC<RepoProps> = ({ label, description, refLink, title }) => {
     <div className="w-full md:w-1/2 lg:w-1/3 p-4">
       <Card className="shadow-lg transition-transform transform hover:scale-105 h-full flex flex-col">
         <CardHeader className="p-3">
-          <CardTitle className="text-lg font-semibold">{label}</CardTitle>
-          <CardDescription className="text-gray-600 mt-1">
-            {description}
-          </CardDescription>
+          <CardTitle className="text-lg font-semibold text-gray-800">{label}</CardTitle>
+          <CardDescription className="text-gray-500 mt-1">{description}</CardDescription>
         </CardHeader>
 
         <div className="mt-auto">
           <CardContent className="p-4 flex-grow border-t border-zinc-300">
             <p className="text-sm font-bold flex items-center justify-between">
               Start with {title}:
-              <Link to="#">{title}</Link>
+              <Link to="#">
+                <Button className="bg-amber-600 text-white hover:bg-orange-400 transition duration-200">Create</Button>
+              </Link>
             </p>
           </CardContent>
 
           <CardFooter className="p-4 border-t border-zinc-300 mt-auto">
-          <Link to={refLink} className="w-full">
-            <Button className="w-full text-white transition duration-200">
-              Learn more
-            </Button>
-          </Link>
-        </CardFooter>
+            <Link to={refLink} className="w-full">
+              <Button className="w-full text-gray-800 bg-gray-200 hover:bg-gray-300 transition duration-200">
+                Learn more
+              </Button>
+            </Link>
+          </CardFooter>
         </div>
       </Card>
     </div>
