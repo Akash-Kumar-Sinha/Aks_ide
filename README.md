dd105a04f04ec467e33663da3c334bc8f538caf5
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2024-11-02 16:51:04 +0530

    dockerized the Aks ide

---
+++ README.md
@@ -0,0 +1,11 @@
+# Welcome to Aks_ide
+
+## Run on localMachine
+
+```shell
+  docker volume create postgres_data
+```
+
+```shell
+  docker-compose up
+```
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  server:{
+    host: true,
+    port: 5173,
+    watch:{
+      usePolling: true
+    }
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
