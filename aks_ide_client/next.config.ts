2896b9b583b74d5ce035785cee79a2f58449fac9
Akash-Kumar-Sinha <akashkumarsinha304@gmail.com>
2026-06-21 01:28:26 +0530

    dockerized the repo

--- aks_ide_client/next.config.ts
+++ aks_ide_client/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  turbopack: {},
   webpack: (config) => {
     config.resolve.fallback = {
       fs: false,
